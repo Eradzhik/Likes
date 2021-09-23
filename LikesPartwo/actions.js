7201a3a98b765b4232815885635b8c52c691f766
Eradzh <ERadzh0777@gmail.com>
2021-09-23 19:03:40 -0400

    initial commit

---
+++ LikesPartwo/actions.js
@@ -0,0 +1,11 @@
+function addMore() {
+    var like = document.querySelector("#likeA")
+    console.log(like);
+    like.innerHTML++
+}
+function moreLikes (){
+    var like = document.querySelector("#likeB")
+    console.log(like)
+    like.innerHTML++
+}
+
---
+++ likes.js
@@ -0,0 +1,6 @@
+let numuserLiked = 3
+let count = document.querySelector("#count")
+function userLiked () {
+    numuserLiked++
+    count.innerText = numuserLiked
+}
